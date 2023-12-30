59bfcdfc53e0e756b7be99a445d4e8ad02da198c
ShoaibTamboli <shoaibtamboli03@gmail.com>
2023-12-30 16:58:54 +0530

    feat & refact: completed UI layout. Refactored some code

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 let rows = 100;
 let columns = 26;
+
 let addressColumnContainer = document.querySelector(
   ".address-column-container"
 );
-
 let addressRowContainer = document.querySelector(".address-row-container");
+let cellsContainer = document.querySelector(".cells-container");
+let addressBar = document.querySelector(".address-bar");
 
 for (let i = 0; i < rows; i++) {
   let addressColumn = document.createElement("div");
   addressColumn.setAttribute("class", "address-column");
   addressColumn.innerText = i + 1;
-
   addressColumnContainer.appendChild(addressColumn);
 }
 
 for (let i = 0; i < columns; i++) {
   let addressRow = document.createElement("div");
   addressRow.setAttribute("class", "address-row");
-  addressRow.innerText = i + 1;
-
+  addressRow.innerText = String.fromCharCode(65 + i);
   addressRowContainer.appendChild(addressRow);
+}
+
+for (let i = 0; i < rows; i++) {
+  let rowContainer = document.createElement("div");
+  rowContainer.setAttribute("class", "row-cell");
+  for (let j = 0; j < columns; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contenteditable", "true");
+    rowContainer.appendChild(cell);
+    addListenerForAddressBarDisplay(cell, i, j);
+  }
+  cellsContainer.appendChild(rowContainer);
+}
+
+function addListenerForAddressBarDisplay(cell, i, j) {
+  cell.addEventListener("click", (e) => {
+    let rowID = i + 1;
+    let columnID = String.fromCharCode(65 + j);
+    addressBar.value = `${columnID}${rowID}`;
+  });
 }
